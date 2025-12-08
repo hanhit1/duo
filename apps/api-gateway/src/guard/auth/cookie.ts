78c5dc2c773fffdacae6d61da07627ebd63e25c7
NgKien94 <quangkienofqn@gmail.com>
2025-12-08 16:41:06 +0700

    fix: sending cookies cross-site

--- apps/api-gateway/src/guard/auth/cookie.ts
+++ apps/api-gateway/src/guard/auth/cookie.ts
@@ -5,7 +5,7 @@ import { GeneratedToken } from '@app/constracts';
 dotenv.config();
 
 // const isLocal = process.env.NODE_ENV === 'local';
-const isLocal = true; // For test purpose, set to true always
+// const isLocal = true; // For test purpose, set to true always
 // const COOKIE_ACCEPT_DOMAIN = process.env.COOKIE_ACCEPT_DOMAIN!;
 
 export const setCookieForFastifyResp = (res: FastifyReply, generatedToken: GeneratedToken) => {
@@ -28,9 +28,10 @@ export const setCookieForFastifyResp = (res: FastifyReply, generatedToken: Gener
 
   res.setCookie('access_token', generatedToken.access_token, {
     httpOnly: true,
-    secure: isLocal ? false : true, // true if use HTTPS
+    // secure: isLocal ? false : true, // true if use HTTPS
+    secure: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day : 1 minute
-    sameSite: 'lax',
+    sameSite: 'none',
     path: '/',
     // domain: isLocal ? undefined : COOKIE_ACCEPT_DOMAIN,
   });
@@ -38,9 +39,10 @@ export const setCookieForFastifyResp = (res: FastifyReply, generatedToken: Gener
   // Set cookie refreshToken
   res.setCookie('refresh_token', generatedToken.refresh_token, {
     httpOnly: true,
-    secure: isLocal ? false : true,
+    // secure: isLocal ? false : true,
+    secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days : 10 minutes
-    sameSite: 'lax',
+    sameSite: 'none',
     path: '/',
     // domain: isLocal ? undefined : COOKIE_ACCEPT_DOMAIN,
   });
@@ -49,15 +51,17 @@ export const setCookieForFastifyResp = (res: FastifyReply, generatedToken: Gener
 export const clearCookieForFastifyResp = (res: FastifyReply) => {
   res.clearCookie('access_token', {
     httpOnly: true,
-    secure: isLocal ? false : true,
-    sameSite: 'lax',
+    // secure: isLocal ? false : true,
+    secure: true,
+    sameSite: 'none',
     path: '/',
     // domain: isLocal ? undefined : COOKIE_ACCEPT_DOMAIN,
   });
   res.clearCookie('refresh_token', {
     httpOnly: true,
-    secure: isLocal ? false : true,
-    sameSite: 'lax',
+    // secure: isLocal ? false : true,
+    secure: true,
+    sameSite: 'none',
     path: '/',
     // domain: isLocal ? undefined : COOKIE_ACCEPT_DOMAIN,
   });
